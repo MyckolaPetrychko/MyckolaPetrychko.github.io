2c9ccc9cdddd0f2a685e641dfd41ecc2b520bfe7
pmv <petrichko96@mail.ru>
2017-10-15 02:43:53 +0300

    Done

--- module1/app.js
+++ module1/app.js
@@ -1,17 +1,26 @@
 (function () {
 	
-	var app = angular.module('LunchCheck', []);
+	var app = angular.module("LunchCheck", []);
 	var ctr = app.controller('LunchCheckController', LunchCheckController);
 
 	LunchCheckController.$inject = ['$scope'];
 
 	function LunchCheckController($scope) {
 		$scope.items = '';
+		$scope.message = '';
 		$scope.btnClick = function () {
-
+			if ($scope.items == '')  {
+				$scope.message = "Please enter data first";
+			} else {
+				var splitted = $scope.items.split(',');
+				var len = splitted.length;
+				if (len <= 3) {
+					$scope.message = "Enjoy!";
+				} else {
+					$scope.message = "Too much!";
+				}
+			}
 		}
 	}
-	//.controller("LunchCheckController", LunchCheckController);
 	
-	
-});
+}());
