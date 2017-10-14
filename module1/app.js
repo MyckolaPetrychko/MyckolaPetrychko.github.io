b379cf01137ae6cc84e911af30cb7d20bcfc2af9
pmv <petrichko96@mail.ru>
2017-10-15 02:14:47 +0300

    Simple app

---
+++ module1/app.js
@@ -0,0 +1,8 @@
+(function () {
+	
+	var app = angular.module('LunchCheck', []);
+	var ctr = app.controller('LunchCheckController', function ($scope) { });
+	//.controller("LunchCheckController", LunchCheckController);
+	
+	
+});
